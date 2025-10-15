71e921d79314421c4ec8c309d441f3fcf6cb01e1
shanyi-ajt <stan@ajobthing.com>
2025-10-15 17:24:28 +0800

    fix: Correct closing tag for Paper component

--- prd-creator/src/app/page.tsx
+++ prd-creator/src/app/page.tsx
@@ -132,7 +132,7 @@ export default function Home() {
                     </li>
                   ))}
                 </ul>
-              </paper>
+              </Paper>
             </Grid>
           )}
         </Grid>
